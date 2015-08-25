c3fc6781e814f6532c0fde1fd951ef2475b676e1
Alex Duda <aduda@fb.com>
2015-08-24 23:40:43 -0700

    General cleanup, compile target update, and adding of testfiles.
    - File name encoding was using UTF_16 leading to weird characters in decoded file names on Unix machines. Switched to UTF_8 which is more standard.
    - Added Javadocs to all methods and inline comments for clarity.
    - Fixed bug where files with no extensions were not encoded properly.
    - Changed compile target to 1.8.
    - Added test file directory.

--- shufflescript/src/FileRandomScript.java
+++ shufflescript/src/FileRandomScript.java
@@ -6,7 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.Base64;
 
-
 /**
  * Class containing the logic for the file obfuscation script
  *
@@ -15,29 +14,44 @@ import java.util.Base64;
  * directory. The files in the new directory will have their names obfuscated. After the user interacts with the files
  * in whatever way they need to, running this script in the reverse direction will de-obfuscate the files. The
  * de-obfuscated files will remain in the new directory to preserve any transformative actions the user may have taken,
- * and the source files will remain unchanged as backups.
+ * and the source files will remain unchanged as backups. The work of obfuscation is done in
+ * {@link #genName(String, String)}
  */
 public class FileRandomScript {
 
     //TODO: Programmatically set the source directory.
-    public static String DIR_PREFIX = "D:\\Users\\Leonid\\Documents\\Science\\";
-    public static String MOD_FOLDER = "\\out";
-    public static String ORIGINAL_FOLDER = "\\in";
+    //public static String DIR_PREFIX = "D:\\Users\\Leonid\\Documents\\Science\\";
+//    public static String MOD_FOLDER = "\\out";
+//    public static String ORIGINAL_FOLDER = "\\in";
+
+    public static String DIR_PREFIX = "/home/aduda/Programming/LonyaLabScripts/testFiles";
+    public static String MOD_FOLDER = "/out";
+    public static String ORIGINAL_FOLDER = "/in";
 
     public static void main(String[] args) {
-        String firstArguement = args[0];
-        if (!firstArguement.equals("r") && !firstArguement.equals("d")) {
+        if (args.length != 1) {
+            System.out.println("Please provide only one argument, r or d");
+            return;
+        }
+
+        String firstArgument = args[0];
+
+        // First decide what direction the user wants us to go
+        if (!firstArgument.equals("r") && !firstArgument.equals("d")) {
             System.out.println("Please enter r or d");
             return;
         }
 
-        if (firstArguement.equals("r")) {
+        // Print the direction in which we're going to the user.
+        if (firstArgument.equals("r")) {
             System.out.println("Randomizing");
         } else {
             System.out.println("Derandomizing");
         }
 
-        String inDirSuffix = (firstArguement.equals("r")) ? ORIGINAL_FOLDER : MOD_FOLDER;
+        // Set up our input and output targets. If we are randomizing them the output folder will be a separate location
+        // from the input folder. If we're derandomizing (de-obfuscating) then we're staying in the same folder.
+        String inDirSuffix = (firstArgument.equals("r")) ? ORIGINAL_FOLDER : MOD_FOLDER;
         File inDir = new File(DIR_PREFIX + inDirSuffix);
         if (!inDir.exists()){
             inDir.mkdir();
@@ -48,57 +62,61 @@ public class FileRandomScript {
             outDir.mkdir();
         }
 
-
-
+        // Process each file
         File[] listOfInFiles = inDir.listFiles();
+        System.out.println("Beginning process on " + listOfInFiles.length + " files from " + inDir.getAbsolutePath());
         for (File inFile : listOfInFiles) {
             String fileFullName = inFile.getName();
+            System.out.println("Processing " + fileFullName);
 
+            // We only want to obfuscate the filename, the file extension needs to be extracted and added back in later
             String[] fileNameArray = fileFullName.split("\\.");
-
-
-            String fileName = "";
-            for (int i = 0; i < fileNameArray.length -1; i++){
-                if (i != 0 ){
-                    fileName +=".";
-                }
-                fileName += fileNameArray[i];
-
+            String fileName = fileNameArray[0];
+            for (int i = 1; i < fileNameArray.length - 1; i++){
+                fileName += "." + fileNameArray[i];
             }
 
-            String fileExt =  fileNameArray[fileNameArray.length -1];
+            // If the file name array only has one element, then there's no file extension
+            String fileExt = "";
+            if (fileNameArray.length > 1) {
+                fileExt = "." + fileNameArray[fileNameArray.length - 1];
+            }
 
-            String newFileName = genName(fileName, firstArguement) + "." + fileExt;
-            File outFile = new File(outDir.getAbsoluteFile() + "\\" + newFileName);
+            String newFileName = genName(fileName, firstArgument) + fileExt;
+            File outFile = new File(outDir.getAbsoluteFile() + "/" + newFileName);
 
-            if (firstArguement.equals("r")) {
+            if (firstArgument.equals("r")) {
                 copyFile(inFile, outFile);
-            }else  {
+            } else {
                 moveFile(inFile, outFile);
             }
-
         }
+    }
 
-            }
-
-
+    /**
+     * Generate the new name of the given file based on the direction in which we're going.
+     * @param fileName filename to alter
+     * @param direction "r" for randomize or "d" for derandomize
+     * @return final filename
+     */
     public static String genName (String fileName, String direction) {
         String editedName = null;
         if (direction.equals("r")) {
-            byte[] encoded = fileName.getBytes(StandardCharsets.UTF_16);
+            byte[] encoded = fileName.getBytes(StandardCharsets.UTF_8);
             editedName = Base64.getEncoder().encodeToString(encoded);
-            byte[] encodedagain = editedName.getBytes(StandardCharsets.UTF_16);
             return editedName;
-
         } else {
             byte[] decoded = Base64.getDecoder().decode(fileName);
-            String decodedName = new String(decoded, StandardCharsets.UTF_16);
+            String decodedName = new String(decoded, StandardCharsets.UTF_8);
             return decodedName;
         }
-
     }
 
-
+    /**
+     * Copies the input file to the location of the output file. Informs the user if the file already exists.
+     * @param inFile location of the input file
+     * @param outFile location of the output file
+     */
     public static void copyFile(File inFile, File outFile) {
         try {
             Files.copy(inFile.toPath(), outFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
@@ -106,20 +124,22 @@ public class FileRandomScript {
             System.out.println("The File Already Exists");
         } catch (Exception e){
             System.out.println("Unknown error");
-                e.printStackTrace();
-
+            e.printStackTrace();
         }
     }
 
+    /**
+     * Copies the input file to the location of the output file. Informs the user if the file already exists.
+     * @param inFile location of the input file
+     * @param outFile location of the output file
+     */
     public static void moveFile(File inFile, File outFile) {
         try {
             Files.move(inFile.toPath(), outFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
-            System.out.println("Input Output Exception occured while moving" + outFile.getName());
+            System.out.println("IOException occured while moving " + outFile.getName());
             e.printStackTrace();
         }
-
-
     }
 
 }
