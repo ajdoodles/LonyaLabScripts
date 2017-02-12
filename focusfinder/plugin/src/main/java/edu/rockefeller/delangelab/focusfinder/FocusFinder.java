3e79bd48e8f5fddb316f27c5aa74b05c6ba4bcf5
Alex Duda <alexjduda@gmail.com>
2017-02-12 12:09:02 -0800

    Add basic and clunky input harvesting to plugin

--- focusfinder/plugin/src/main/java/edu/rockefeller/delangelab/focusfinder/FocusFinder.java
+++ focusfinder/plugin/src/main/java/edu/rockefeller/delangelab/focusfinder/FocusFinder.java
@@ -1,9 +1,12 @@
 package edu.rockefeller.delangelab.focusfinder;
 
+import net.imagej.ImageJ;
 import org.scijava.ItemIO;
 import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
+
+import java.io.File;
 
 /**
  * Plugin that provides the focus finder functionality.
@@ -11,14 +14,67 @@ import org.scijava.plugin.Plugin;
 @Plugin(type = Command.class, menuPath = "Plugins>Focus Finder")
 public class FocusFinder implements Command {
 
-    @Parameter(label = "What is your name?")
-    private String name = "J. Doe";
+    @Parameter(label = "Input Directory", style = "directory")
+    private File inputDir;
 
-    @Parameter(type = ItemIO.OUTPUT)
-    private String greeting;
+    @Parameter(label = "Output Directory", style = "directory")
+    private File outputDir;
+
+    @Parameter(label = "How many channels?", choices = {"1", "2", "3", "4"}, style ="radioButtonHorizontal")
+    private String numChannelsString = "3";
+
+    @Parameter(label = "Which channel is DAPI?", choices = {"1", "2", "3", "4"}, style ="radioButtonHorizontal")
+    private String dapiChannelString = "3";
+
+    @Parameter(label = "Coloc 1?")
+    private boolean shouldColoc1;
+
+    @Parameter(label = "Coloc 2?")
+    private boolean shouldColoc2;
+
+    @Parameter(label = "Coloc 3?")
+    private boolean shouldColoc3;
+
+    @Parameter(label = "Coloc 4?")
+    private boolean shouldColoc4;
+
+    @Parameter(label = "Use autocontrast on first foci?")
+    private boolean shouldAutoContrastFirstFoci;
+
+    @Parameter(label = "Enter min value")
+    private int firstFociAutoContrastMin = 0;
+
+    @Parameter(label = "Enter max value")
+    private int firstFociAutoContrastMax = 255;
+
+    @Parameter(label = "Use autocontrast on second foci?")
+    private boolean shouldAutoContrastSecondFoci;
+
+    @Parameter(label = "Enter min value")
+    private int secondFociAutoContrastMin = 0;
+
+    @Parameter(label = "Enter max value")
+    private int secondFociAutoContrastMax = 255;
+
+    @Parameter(label = "Threshold value for first foci")
+    private int firstFociThreshold = -180;
+
+    @Parameter(label = "Threshold value for second foci")
+    private int secondFociThreshold = -180;
+
+    private int numChannels;
+    private int dapiChannel;
+    private boolean[] channelsToColoc;
 
     @Override
     public void run() {
-        greeting = "Hello " + name + ".";
+        numChannels = Integer.valueOf(numChannelsString);
+        dapiChannel = Integer.valueOf(dapiChannelString);
+        channelsToColoc = new boolean[] {shouldColoc1, shouldColoc2, shouldColoc3, shouldColoc4};
+    }
+
+    public static final void main(String[] args) {
+        final ImageJ ij = net.imagej.Main.launch(args);
+        ij.command().run(FocusFinder.class, true);
     }
 }
